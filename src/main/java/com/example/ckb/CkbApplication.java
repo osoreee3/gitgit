cdfbf529258402ef788a385c3b08a70a2f236daa
osoreee3 <taecoo3@naver.com>
2023-07-07 09:48:20 +0900

    start

---
+++ src/main/java/com/example/ckb/CkbApplication.java
@@ -0,0 +1,13 @@
+package com.example.ckb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkbApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/ckb/MainController.java
@@ -0,0 +1,8 @@
+package com.example.ckb;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+}
